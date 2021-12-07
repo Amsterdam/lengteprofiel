c6092a67ed963986695bff0cca1f198e3774be8d
Thomas van der Linden <t.van.der.linden@amsterdam.nl>
2021-12-07 14:26:59 +0100

    change of directory

--- geotechnisch_lengteprofiel.py
+++ geotechnisch_lengteprofiel.py
@@ -5,9 +5,9 @@ Geschreven door Thomas van der Linden, Ingenieursbureau Amsterdam
 
 Input:
 - sonderingen, als XML of GEF in een map met de naam cpts in dezelfde directory als de code
-- profiellijn, als variabele
+- profiellijn, als geojson bestand
 - lagen, als excel tabel met de naam layers.xlsx in dezelfde directory als de code
-    de tabel heeft kolommen laag (nummer in dezelfde volgorde als je getekend hebt), materiaal, kleur (in het Engels)
+    de tabel heeft kolommen laag (nummer in dezelfde volgorde als je getekend hebt), materiaal, plotkleur (in het Engels)
 
 Output:
 - output.geo dat ingelezen kan worden in de D-Serie # TODO: materialen toevoegen
@@ -23,19 +23,28 @@ Afhankelijkheden
 - standaard Python packages
 """
 
+
+# TODO: ordenen, methods onderbrengen in classes
+# TODO: kan het ook runnen zonder eerst een canvas, maar direct een plaatje
+
+
 import numpy as np
 import pandas as pd
 from shapely.geometry import Point, LineString
 import os
 import sys
+import json
 import tkinter as tk
 from datetime import datetime
 import matplotlib.pyplot as plt
 
 sys.path.insert(0, '..\\cpt_viewer')
-from cpt_reader import Cpt
+from cpt_reader import Cpt, Bore
 
-line = LineString([(127743, 485659), (127880, 485541)]) # Deze moet je zelf opgeven
+line = LineString([(120097, 487385), (119981, 487339), (119935, 487200), (119908, 487040), (120099, 486579)]) # Deze moet je zelf opgeven
+
+with open ("./profiel.geojson") as f:
+    line = LineString(json.loads(f.read())["features"][0]["geometry"]["coordinates"])
 
 class Cptverzameling():
     def __init__(self):
@@ -50,13 +59,26 @@ class Cptverzameling():
             elif f.lower().endswith("gef"):
                 cpt.load_gef(f)
                 self.cpts.append(cpt)
-            
+
+class Boreverzameling():
+    def __init__(self):
+        self.bores = []
+    
+    def load_multi_bore(self, fileList):
+        for f in fileList:
+            bore = Bore()
+            if f.lower().endswith("xml"):
+                bore.load_xml(f)
+                self.bores.append(bore)
 
 class GeotechnischLengteProfiel():
     def __init__(self):
         self.line = None
         self.cpts = None
         self.materials = None
+        self.boundaries = {}
+        self.groundlevelRel = []
+        self.groundlevelAbs = []
 
     def set_line(self, line):
         self.line = line
@@ -64,27 +86,59 @@ class GeotechnischLengteProfiel():
     def set_cpts(self, cptVerzameling):
         self.cpts = cptVerzameling.cpts
 
-    def set_boundaries(self, boundaries):
-        self.boundaries = boundaries
+    def set_bores(self, boreVerzameling):
+        self.bores = boreVerzameling.bores
 
     def set_layers(self, materialsTable):
         materials = pd.read_excel(materialsTable, index_col="laag")
         self.materials = materials
-
-    def project_cpts_on_line(self):
+     
+    def project_on_line(self):
         for cpt in self.cpts:
             cptLocation = Point(cpt.easting, cpt.northing)
             cpt.projectedLocation = self.line.project(cptLocation, normalized=True)
+        for bore in self.bores:
+            boreLocation = Point(bore.easting, bore.northing)
+            bore.projectedLocation = self.line.project(boreLocation, normalized=True)
 
-fileList = os.listdir("./cpts/")
-fileList = ["./cpts/" + f for f in fileList]
+    def set_groundlevel(self):
+        # TODO: dit is niet zo mooi
+
+        for bore in self.bores:
+            self.groundlevelRel.append([bore.projectedLocation, bore.groundlevel])
+            self.groundlevelAbs.append([bore.projectedLocation * self.line.length, bore.groundlevel])
+        for cpt in self.cpts:
+            self.groundlevelRel.append([cpt.projectedLocation, cpt.groundlevel])
+            self.groundlevelAbs.append([cpt.projectedLocation * self.line.length, cpt.groundlevel])
+
+        self.groundlevelAbs.sort()
+        self.groundlevelAbs.insert(0, [0, self.groundlevelAbs[0][1]])
+        self.groundlevelAbs.append([self.line.length, self.groundlevelAbs[-1][1]])
+        self.groundlevelAbs = np.asarray(self.groundlevelAbs)
+
+        self.groundlevelRel.sort()
+        self.groundlevelRel.insert(0, [0, self.groundlevelRel[0][1]])
+        self.groundlevelRel.append([self.line.length, self.groundlevelRel[-1][1]])
+        self.groundlevelRel = np.asarray(self.groundlevelRel)
+
+cptPath = "./cpts/"
+
+fileList = os.listdir(cptPath)
+fileList = [cptPath + f for f in fileList]
 multicpt = Cptverzameling()
 multicpt.load_multi_cpt(fileList)
+
+fileList = os.listdir("./bores/")
+fileList = ["./bores/" + f for f in fileList]
+multibore = Boreverzameling()
+multibore.load_multi_bore(fileList)
 
 gtl = GeotechnischLengteProfiel()
 gtl.set_line(line)
 gtl.set_cpts(multicpt)
-gtl.project_cpts_on_line()
+gtl.set_bores(multibore)
+gtl.project_on_line()
+gtl.set_groundlevel()
 gtl.set_layers("./layers.xlsx")
 
 def plot(gtl, boundaries):
@@ -92,11 +146,21 @@ def plot(gtl, boundaries):
 
     # plot de cpts
     for cpt in gtl.cpts:
-        qcX = cpt.data["coneResistance"] / 10 + cpt.projectedLocation * gtl.line.length # TODO: ipv 10 een factor afhankelijk van aantal cpts en tussenafstand?
-        rfX = cpt.data["frictionRatio"] / 10 + cpt.projectedLocation * gtl.line.length # TODO: ipv 10 een factor afhankelijk van aantal cpts en tussenafstand?
+        qcX = cpt.data["coneResistance"] / 2 + cpt.projectedLocation * gtl.line.length # TODO: ipv een vaste waarde een factor afhankelijk van aantal cpts en tussenafstand?
+        rfX = cpt.data["frictionRatio"] / 2 + cpt.projectedLocation * gtl.line.length # TODO: ipv vaste waarde een factor afhankelijk van aantal cpts en tussenafstand?
         y = -1 * cpt.data["depth"] + cpt.groundlevel
         plt.plot(qcX, y, c="blue", linewidth=0.5)
         plt.plot(rfX, y, c="green", linewidth=0.5)
+        plt.text(qcX.min(), y.max(), cpt.testid)
+        plt.grid(b=True)
+        plt.minorticks_on()
+        plt.grid(b=True, which="minor", lw=0.1)
+
+    # plot de boringen
+    for bore in gtl.bores:
+        boreX = bore.projectedLocation * gtl.line.length
+        for i, layer in bore.soillayers.iterrows():
+            plt.plot([boreX, boreX], [layer.upper_NAP, layer.lower_NAP], layer.plotColor)
 
     # plot de laaggrenzen als lijnen
     for boundary, points in boundaries.items():
@@ -130,41 +194,41 @@ def plot(gtl, boundaries):
     ax1.set_xlabel("afstand [m]")
     ax1.set_ylabel("niveau [m t.o.v. NAP]")
 
-    plt.savefig("./gtl.svg")
-    plt.savefig("./gtl.png")   
+    plt.savefig("./gtl.svg", bbox_inches="tight")
+    plt.savefig("./gtl.png", bbox_inches="tight")   
 
 def add_line(eventorigin):
     # deze functie maakt het mogelijk om een lijn (grens) toe te voegen 
     global i
-    i += 1
+    i += 1 # TODO: dit zou toch via gtl.boundaries moeten kunnen?
 
 tklines = []
 def draw_line(eventorigin):
     # deze functie maakt het mogelijk om een lijn (grens) te tekenen
-    global boundaries, tkline
+    global tkline
     global x,y
     x = eventorigin.x
     y = eventorigin.y
     
     # check of er een nieuwe lijn is begonnen
     # dan deze toevoegen aan de verzameling
-    if i not in boundaries.keys():
+    if i not in gtl.boundaries.keys():
         # voeg het geklikte punt toe aan de lijn
-        boundaries[i] = [[x,y]]
+        gtl.boundaries[i] = [[x,y]]
     else:
         # voeg het geklikte punt toe aan de lijn
-        boundaries[i].append([x,y])
+        gtl.boundaries[i].append([x,y])
     # teken de lijn (als er meer dan 1 punt is)
-    if len(boundaries[i]) > 1:
-        tkline = C.create_line(boundaries[i])
+    if len(gtl.boundaries[i]) > 1:
+        tkline = C.create_line(gtl.boundaries[i])
         tklines.append(tkline)
 
 def remove_last_point(eventorigin):
     # deze functie verwijdert het laatste punt van de laatste boundary (kan meerdere punten verwijderen)
     # en verwijdert het laatst getekende lijnstuk
-    last_boundary = max(boundaries.keys())
-    if len(boundaries[last_boundary]) > 0:
-        del boundaries[last_boundary][-1]
+    last_boundary = max(gtl.boundaries.keys())
+    if len(gtl.boundaries[last_boundary]) > 0:
+        del gtl.boundaries[last_boundary][-1]
         C.delete(tklines[-1])
         del tklines[-1]
 
@@ -174,12 +238,18 @@ def finish():
     root.quit()
 
     # verwijder het laatste punt van boundaries, dat ontstaat bij het sluiten
-    boundaries[max(boundaries.keys())] = boundaries[max(boundaries.keys())][:-1]
+    gtl.boundaries[max(gtl.boundaries.keys())] = gtl.boundaries[max(gtl.boundaries.keys())][:-1]
     
-    yScale, top = get_yscale_for_canvas(multicpt, canvasHeight)
+    yScale, top = get_yscale_for_canvas(gtl.cpts, gtl.bores, canvasHeight)
     realLength = line.length
-    scaledBoundaries = scale_points_to_real_world(boundaries, realLength, canvasWidth, yScale, top)
+    scaledBoundaries = scale_points_to_real_world(gtl.boundaries, realLength, canvasWidth, yScale, top)
     boundariesModifiedLimits = modify_geometry_limits(scaledBoundaries, 0, realLength)
+    
+    # voeg het maaiveld toe
+    boundariesModifiedLimits[1] = gtl.groundlevelAbs.tolist()
+
+    # TODO: eigenlijk moeten de gtl.boundaries worden vervangen door de boundariesModifiedLimits 
+    
     write_to_DSerie_input(boundariesModifiedLimits)
     plot(gtl, boundariesModifiedLimits)
 
@@ -284,13 +354,16 @@ def scale_points_to_real_world(boundaries, realLength, canvasWidth, yScale, top)
         scaledBoundaries[boundary] = [[scaledX, scaledY] for scaledX, scaledY in list(zip(scaledX, scaledY))]
     return scaledBoundaries
 
-def get_yscale_for_canvas(multicpt, canvasHeight):
+def get_yscale_for_canvas(cpts, bores, canvasHeight):
     # volgende blok bepaalt de yscale
     groundlevels = []
     finaldepths = []
-    for cpt in multicpt.cpts:
+    for cpt in cpts:
         groundlevels.append(cpt.groundlevel)
         finaldepths.append(cpt.finaldepth)
+    for bore in bores:
+        groundlevels.append(bore.groundlevel)
+        finaldepths.append(bore.finaldepth) 
     top = max(groundlevels)
     length = max(finaldepths)
     yscale = canvasHeight / length
@@ -303,28 +376,56 @@ def scale_cpt_to_canvas(cpt, xScaleCPT, yscale, groundlevel, top, width):
     y = (top - groundlevel + cpt.data["depth"]) * yscale 
     return qcX, rfX, y
 
-def create_canvas_with_cpts(multicpt, canvasWidth, canvasHeight, xScaleCPT):
+def scale_bore_to_canvas(bore, yscale, groundlevel, top, width):
+    xs = np.full(len(bore.soillayers.index), bore.projectedLocation * width)
+    uppers = (top - groundlevel + bore.soillayers["upper"]) * yscale 
+    lowers = (top - groundlevel + bore.soillayers["lower"]) * yscale 
+    return xs, uppers, lowers
+
+def scale_groundlevel_to_canvas(gtl, yscale, top, width):
+    xs = gtl.groundlevelRel[:,0] * width
+    ys = (gtl.groundlevelRel[:,1] * -1 + top) * yscale
+    return xs, ys
+
+def create_canvas_with_cpts(gtl, canvasWidth, canvasHeight, xScaleCPT):
     # deze functie maakt een canvas met daarop plots van cpts
 
     # maak het canvas
-    global C, boundaries, i, root
+    global C, i, root
     root = tk.Tk()
     C = tk.Canvas(root, height=canvasHeight, width=canvasWidth)
 
     # bepaal de schaal voor de verticale as
-    yscale, top = get_yscale_for_canvas(multicpt, canvasHeight)
+    yscale, top = get_yscale_for_canvas(gtl.cpts, gtl.bores, canvasHeight)
 
     # voeg de cpts toe aan het canvas
-    for i, cpt in enumerate(multicpt.cpts):
+    for i, cpt in enumerate(gtl.cpts):
         qcX, rfX, y = scale_cpt_to_canvas(cpt, xScaleCPT, yscale, cpt.groundlevel, top, canvasWidth)
         # tkinter moet een lijst van x,y punten hebben
         qcPoints = list(zip(qcX, y))
         rfPoints = list(zip(rfX, y))
         qcLine = C.create_line(qcPoints, fill="blue")
         rfLine = C.create_line(rfPoints, fill="green")
+        cptText = C.create_text(qcPoints[0], text=cpt.testid)
 
-    boundaries = {}
-    i = 1
+    # voeg de boringen toe aan het canvas
+    for i, bore in enumerate(gtl.bores):
+
+        xs, uppers, lowers = scale_bore_to_canvas(bore, yscale, bore.groundlevel, top, canvasWidth)
+        colors = bore.soillayers["plotColor"]
+
+        for x, upper, lower, color in list(zip(xs, uppers, lowers, colors)):
+            point_from = (x, upper)
+            point_to = (x, lower)
+            print(point_from, point_to)
+            layer_line = C.create_line(x, upper, x, lower, fill=color, width=5)
+
+    # voeg het maaiveld toe aan het canvas
+    xs, ys = scale_groundlevel_to_canvas(gtl, yscale, top, canvasWidth)
+    groundlevelPoints = list(zip(xs, ys))
+    groundlevel_line = C.create_line(groundlevelPoints)
+
+    i = 2
     quitbutton = tk.Button(root, text="Quit", command=finish).pack()
 
     C.pack()
@@ -346,4 +447,4 @@ canvasWidth = 1800 # breedte van canvas
 canvasHeight = 750 # hoogte van canvas
 xScaleCPT = 5 # horizontale schaal voor lijnen van cpt op canvas
 
-create_canvas_with_cpts(multicpt, canvasWidth, canvasHeight, xScaleCPT)
+create_canvas_with_cpts(gtl, canvasWidth, canvasHeight, xScaleCPT)
