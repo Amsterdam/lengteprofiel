c4a2bba6db3c0bd7fbad277e4697ccc7973eac91
Thomas van der Linden <t.van.der.linden@amsterdam.nl>
2022-05-19 16:29:16 +0200

    veld toegevoegd aan soillayers, vanwege wijziging gefxml_viewer

--- geotechnisch_lengteprofiel.py
+++ geotechnisch_lengteprofiel.py
@@ -147,7 +147,7 @@ class GeotechnischLengteProfiel():
         colorsDict = {1: "yellow", 4: "brown", 2: "steelblue", 0: "gray", 5: "lime", 3: "purple", 6: "black"}
         for bore in self.bores:
             boreX = bore.projectedLocation * self.line.length
-            for i, layer in bore.soillayers.iterrows():
+            for i, layer in bore.soillayers['veld'].iterrows(): # plot alleen de veldbeschrijving, deze is als optie toegevoegd rond 19 mei 2022 aan de gefxml_viewer
                 mainMaterial = layer.components[max(layer.components.keys())]
                 plotColor = colorsDict[mainMaterial]
                 plt.plot([boreX, boreX], [layer.upper_NAP, layer.lower_NAP], plotColor, lw=4) # TODO: xml boringen hebben een attribute plotColor, dat is niet meer nodig
@@ -464,9 +464,9 @@ class GeotechnischLengteProfiel():
         return qcX, rfX, y
 
     def scale_bore_to_canvas(self, bore, yscale, groundlevel, top, width):
-        xs = np.full(len(bore.soillayers.index), bore.projectedLocation * width)
-        uppers = (top - groundlevel + bore.soillayers["upper"]) * yscale 
-        lowers = (top - groundlevel + bore.soillayers["lower"]) * yscale 
+        xs = np.full(len(bore.soillayers['veld'].index), bore.projectedLocation * width)
+        uppers = (top - groundlevel + bore.soillayers['veld']["upper"]) * yscale 
+        lowers = (top - groundlevel + bore.soillayers['veld']["lower"]) * yscale 
         return xs, uppers, lowers
 
     def scale_groundlevel_to_canvas(self, yscale, top, width):
@@ -500,7 +500,7 @@ class GeotechnischLengteProfiel():
         for i, bore in enumerate(self.bores):
 
             xs, uppers, lowers = self.scale_bore_to_canvas(bore, yscale, bore.groundlevel, top, self.canvasWidth)
-            colors = bore.soillayers["plotColor"]
+            colors = bore.soillayers['veld']["plotColor"]
 
             for x, upper, lower, color in list(zip(xs, uppers, lowers, colors)):
                 point_from = (x, upper)
